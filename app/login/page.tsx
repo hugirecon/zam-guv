20f78b9db73137d753bf3ab625097a12c5dd04e1
KDT Super App <kdtsuperapp@Hugi-3.local>
2026-05-15 08:24:38 -0400

    Update email placeholder to firstname.lastname@kdt.guv

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -141,7 +141,7 @@ Back to Home
                 required
                 autoComplete="email"
                 className="w-full border border-gray-300 rounded-lg px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-shadow"
-                placeholder="you@company.com"
+                placeholder="firstname.lastname@kdt.guv"
               />
             </div>
 
